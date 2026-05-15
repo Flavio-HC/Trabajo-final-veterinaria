4375c265504d95679865f0f2ef2f8c802f1659ef
Gadiel Mateo González Pamo <gadiel.gonzalezp@gmail.com>
2026-05-14 19:34:38 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -12,3 +12,16 @@ CREATE TABLE ciudad (
     nombre_ciudad VARCHAR(100) NOT NULL,
     codigo_ubigeo VARCHAR(10)
 );
+CREATE TABLE cliente (
+    id_cliente SERIAL PRIMARY KEY,
+    dni VARCHAR(20) NOT NULL UNIQUE,
+    nombre VARCHAR(100) NOT NULL,
+    apellidos VARCHAR(100) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    direccion VARCHAR(255),
+    id_ciudad INT NOT NULL,
+
+    CONSTRAINT fk_cliente_ciudad
+        FOREIGN KEY (id_ciudad)
+        REFERENCES ciudad(id_ciudad)
+);
