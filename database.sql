f10c835ad86de92de4483fd95c94b26e6c02e73a
Gadiel Mateo González Pamo <gadiel.gonzalezp@gmail.com>
2026-05-16 17:24:04 -0500

    feat Se agrego los atributos de tratamiento, receta y medicamento

--- database.sql
+++ database.sql
@@ -54,3 +54,37 @@ CREATE TABLE pago (
     FOREIGN KEY (id_cliente)
     REFERENCES cliente(id_cliente)
 );
+
+CREATE TABLE tratamiento (
+    id_tratamiento SERIAL PRIMARY KEY,
+    descripcion TEXT NOT NULL,
+    duracion VARCHAR(50) NOT NULL,
+    estado VARCHAR(20),
+    id_diagnostico INT NOT NULL,
+
+    CONSTRAINT fk_tratamiento_diagnostico
+        FOREIGN KEY (id_diagnostico)
+        REFERENCES diagnostico(id_diagnostico)
+);
+CREATE TABLE receta (
+    id_receta SERIAL PRIMARY KEY,
+    dosis VARCHAR(100) NOT NULL,
+    frecuencia VARCHAR(100) NOT NULL,
+    duracion VARCHAR(100) NOT NULL,
+    id_tratamiento INT NOT NULL,
+    id_medicamento INT NOT NULL,
+
+    CONSTRAINT fk_receta_tratamiento
+        FOREIGN KEY (id_tratamiento)
+        REFERENCES tratamiento(id_tratamiento),
+    CONSTRAINT fk_receta_medicamento
+        FOREIGN KEY (id_medicamento)
+        REFERENCES medicamento(id_medicamento)
+);
+CREATE TABLE medicamento (
+    id_medicamento SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    stock INT NOT NULL,
+    precio_unitario DECIMAL(10,2) NOT NULL,
+    fecha_vencimiento DATE NOT NULL
+);
