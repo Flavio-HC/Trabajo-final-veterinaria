6ff47473c9561aed23a7fcbbd7b7ef056179b77c
Gadiel Mateo González Pamo <gadiel.gonzalezp@gmail.com>
2026-05-21 09:27:26 -0500

    feat gadiel, se pobló la tabla especie y raza

--- registro.sql
+++ registro.sql
@@ -1,1 +1,13 @@
+INSERT INTO especie (nombre_especie, descripcion) VALUES
+('Perro', 'Caninos domésticos'),
+('Gato', 'Felinos domésticos'),
+('Conejo', 'Mamíferos pequeños'),
+('Ave', 'Aves domésticas'),
+('Hamster', 'Roedores pequeños');
 
+INSERT INTO raza (nombre_raza, id_especie) VALUES
+('Labrador', 1),
+('Pastor Alemán', 1),
+('Siamés', 2),
+('Persa', 2),
+('Conejo Enano', 3);
