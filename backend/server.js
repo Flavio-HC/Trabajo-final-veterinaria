bd9b63bb8e907c370b632868f9f994456110682c
Flavio-HC <faxintowers@gmail.com>
2026-06-10 17:45:12 -0500

    feat: conectar rutas de reportes al servidor

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Backend veterinaria");
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const reportes = require('./routes/reportes');
+
+app.use('/reportes', reportes);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en puerto ${PORT}`);
+});
